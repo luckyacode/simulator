765e53b19aa16d9e713d9c899dd65e5a2400973f
DHEERENDRA <lifesstudy@gmail.com>
2026-06-16 10:02:10 +0530

    fix

--- src/main/java/com/praveen/simulator/helper/ExceptionHandlers.java
+++ src/main/java/com/praveen/simulator/helper/ExceptionHandlers.java
@@ -1,15 +1,18 @@
 package com.praveen.simulator.helper;
 
 import com.praveen.simulator.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionHandlers {
 
     @ExceptionHandler({AirlineException.class})
     public ResponseEntity<ApiResponse<Throwable>> handleException(AirlineException airlineException){
+        log.error("Can not process further due to {}",airlineException.getMessage());
         return ApiResponse.error(airlineException.getMessage());
     }
 }
